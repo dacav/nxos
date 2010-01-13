a41583e34322c26e122cf85ca2f46ed9aa02a38f
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2010-01-13 09:47:42 +0100

    Experimental page loading

--- rorom_utils/src/loadflash.c
+++ rorom_utils/src/loadflash.c
@@ -18,7 +18,6 @@
  *
  */
 
-#include <libusb-1.0/libusb.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,116 +26,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/* Various constants, NXOS related */
-#define TX_ENDPOINT 1
-#define TX_TIMEOUT  0
-#define RX_ENDPOINT 2
-#define RX_TIMEOUT  0   
-#define USB_BUFSIZE 64  // nxos usb buffer size
-
-/* USB Parameters of NXT/NxOS */
-static const unsigned NXT_VENDOR_ID  = 0x0694;
-static const unsigned NXT_PRODUCT_ID = 0xff00;
-
-/* USB Parameters of Sam-ba */
-static const unsigned SAM_VENDOR_ID  = 0x03eb;
-static const unsigned SAM_PRODUCT_ID = 0x6124;
-
-typedef struct {
-    libusb_context *context;
-    libusb_device_handle *handle;
-} nxt_link_t;
-
-static
-const char *libusb_get_error (int err)
-{
-    switch (err) {
-        case LIBUSB_SUCCESS:
-            return "Success";
-        case LIBUSB_ERROR_IO:
-            return "I/O error";
-        case LIBUSB_ERROR_INVALID_PARAM:
-            return "Invalid param";
-        case LIBUSB_ERROR_ACCESS:
-            return "Access error";
-        case LIBUSB_ERROR_NO_DEVICE:
-            return "Device not found";
-        case LIBUSB_ERROR_NOT_FOUND:
-            return "Entity not found";
-        case LIBUSB_ERROR_BUSY:
-            return "Resource busy";
-        case LIBUSB_ERROR_TIMEOUT:
-            return "Timed out";
-        case LIBUSB_ERROR_OVERFLOW:
-            return "Overflow";
-        case LIBUSB_ERROR_PIPE:
-            return "Pipe error";
-        case LIBUSB_ERROR_INTERRUPTED:
-            return "Interrupted";
-        case LIBUSB_ERROR_NO_MEM:
-            return "Not enough memory";
-        case LIBUSB_ERROR_NOT_SUPPORTED:
-            return "Operation not supported";
-        case LIBUSB_ERROR_OTHER:
-        default:
-            return "Unknown error";
-    }
-}
-
-static
-int usb_init (nxt_link_t *nxt)
-{
-    libusb_context *context;
-    libusb_device_handle *handle;
-    int err;
-
-    if ((err = libusb_init(&context)) != LIBUSB_SUCCESS) {
-        fprintf(stderr, "LibUsb: %s\n", libusb_get_error(err));
-        return 1;
-    }
- 
-    handle = libusb_open_device_with_vid_pid(context, NXT_VENDOR_ID,
-             NXT_PRODUCT_ID);
-    if (handle == NULL) {
-        fprintf(stderr, "Opening NXT: ");
-        
-        handle = libusb_open_device_with_vid_pid(context, SAM_VENDOR_ID,
-                 SAM_PRODUCT_ID);
-        if (handle != NULL) { 
-            libusb_close(handle);
-            libusb_exit(context);
-            fprintf(stderr, "First of all load NxOS\n");
-        } else {
-            fprintf(stderr, "Device not found\n");
-        }
-        return 1;
-    }
-
-    nxt->context = context;
-    nxt->handle = handle;
-    return 0;
-}
-
-static
-void usb_free (nxt_link_t *nxt)
-{
-    libusb_close(nxt->handle);
-    libusb_exit(nxt->context);
-}
-
-static
-int usb_send (nxt_link_t *nxt, void *buffer, size_t len)
-{
-    int err;
-    int32_t transf;
-
-    if ((err = libusb_bulk_transfer(nxt->handle, TX_ENDPOINT, buffer,
-                    len, &transf, TX_TIMEOUT)) != LIBUSB_SUCCESS) {
-        fprintf(stderr, "Data transfer: %s\n", libusb_get_error(err));
-        transf = -1;
-    }
-    return transf;
-}
+#include "nxt.h"
 
 static
 int get_ro_size (const char *path)
@@ -173,26 +63,26 @@ int main (int argc, char **argv)
     uint8_t buffer[USB_BUFSIZE];
     size_t read, sent;
 
-    if (usb_init(&nxt)) {
+    if (nxt_init(&nxt)) {
         exit(1);
     }
 
     FILE *ro_data = fopen(filename, "rb");
     if (ro_data == NULL) {
         fprintf(stderr, "Error in opening %s\n", filename);
-        usb_free(&nxt);
+        nxt_free(&nxt);
     }
 
     fprintf(stdout, "Sync sent: %d bytes\n"
                     "           start=%p\n"
                     "           length=%d\n",
-            usb_send(&nxt, (void *)&data, sizeof(tx_data_t)),
+            nxt_send(&nxt, (void *)&data, sizeof(tx_data_t)),
             (void *)data.start_address,
             data.length);
 
     while ((read = fread((void *)buffer, USB_BUFSIZE, 1, ro_data)) > 0) {
         read *= USB_BUFSIZE;
-        sent = usb_send(&nxt, (void *)buffer, read);
+        sent = nxt_send(&nxt, (void *)buffer, read);
         fprintf(stdout, "Remaining: %d\r", data.length);
         if (sent == -1) {
             break;
@@ -207,7 +97,7 @@ int main (int argc, char **argv)
     }
 
     fclose(ro_data);
-    usb_free(&nxt);
+    nxt_free(&nxt);
     exit(0);
 }
 
